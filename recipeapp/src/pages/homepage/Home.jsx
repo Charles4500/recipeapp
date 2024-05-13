226f7ef11d225ee181ecef0f731165d3c07f596e
CharlesKibet <charlesbiegon973@gmail.com>
2024-05-13 10:14:08 +0300

    Api for fetching data updated

--- recipeapp/src/pages/homepage/Home.jsx
+++ recipeapp/src/pages/homepage/Home.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React from 'react';
 import Search from '../../components/Search';
-import './styles.css'
+import './styles.css';
 function Home() {
-  function getDataFromSearchComponent (getData){
-    console.log(getData);
-  }
+  const getDataFromSearchComponent = (getData) => {
+    // console.log(getData);
+    //Data fetching
+    async function getRecipes() {
+      const apiResponse = await fetch(
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=20dbb71b8e104f758aa5284ccd046211&query=${getData}`
+      );
+      const result = await apiResponse.json();
+
+      console.log(result);
+    }
+
+    getRecipes();
+  };
+
   return (
-    <div className='home'>
-      <Search getDataFromSearchComponent = {getDataFromSearchComponent}/>
+    <div className="home">
+      <Search getDataFromSearchComponent={getDataFromSearchComponent} />
     </div>
-  )
+  );
 }
 
 export default Home;
