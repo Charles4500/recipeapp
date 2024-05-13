94b8ec30189426ea4e0d83c0c7d5f4b792a85f05
CharlesKibet <charlesbiegon973@gmail.com>
2024-05-13 09:39:58 +0300

    Passing data from parent to child updated

--- recipeapp/src/components/Search.jsx
+++ recipeapp/src/components/Search.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import './search/style.css'
-function Search() {
+import React, { useState } from 'react';
+import './search/style.css';
+function Search({getDataFromSearchComponent}) {
+  const [inputValue, setInputValue] = useState('');
+  function handleInputChange(event){
+    const {value} =event.target;
+    setInputValue(value)
+    console.log(value);
+  }
+  function handleSubmit(event){
+    event.preventDefault()
+    getDataFromSearchComponent(inputValue)
+  }
+  
   return (
-    <form className='search'>
-      <input name='search' placeholder='Search recipe here' />
-      <button type='submit'>Search</button>
+    <form onSubmit={handleSubmit} className="search">
+      <input
+        name="search"
+        placeholder="Search recipe here"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <button type="submit">Search</button>
     </form>
-  )
+  );
 }
 
-export default Search
+export default Search;
